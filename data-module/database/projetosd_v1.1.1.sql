3df5434a1115e64a0564360771eae3e514406563
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2020-04-21 10:58:08 -0300

    paulo_database [Nao_compila] commit de sincronizacao

--- data-module/database/projetosd_v1.1.1.sql
+++ data-module/database/projetosd_v1.1.1.sql
@@ -2,7 +2,7 @@ use projetosd;
 
 
 create table if not exists pessoa(
-	id int(7) unsigned not null primary key,
+	id int(7) unsigned not null auto_increment primary key,
 	nome varchar(50) not null,
 	sobrenome varchar(50) not null,
 	data_nascimento DATE,
@@ -12,23 +12,24 @@ create table if not exists pessoa(
 );
 
 create table if not exists endereco(
-    id int(7) unsigned not null primary key,
+    id int(7) unsigned not null auto_increment primary key,
     rua varchar(50) not null,
     numero varchar(10) not null ,
-    cep int(10) unsigned,
+    complemento varchar(50),
+    cep varchar(10),
     bairro varchar(35),
     cidade varchar(50) not null,
     estado varchar(35)
 );
 
 create table if not exists atendente(
-	id int(7) unsigned not null primary key,
+	id int(7) unsigned not null auto_increment primary key,
 	codigo_cracha varchar(35),
 	constraint fk_atendente_pessoa foreign key(id) references pessoa(id)
 );
 
 create table if not exists paciente(
-    id int(7) unsigned not null primary key,
+    id int(7) unsigned not null auto_increment primary key,
     endereco_id int(7) unsigned not null,
     pcd bit(1) default 0,
     constraint fk_paciente_pessoa foreign key(id) references pessoa(id),
@@ -36,18 +37,17 @@ create table if not exists paciente(
 );
 
 create table if not exists agendamento(
-    id int(7) unsigned not null primary key,
+    id int(7) unsigned not null auto_increment primary key,
     paciente_id int(7) unsigned not null,
     data_agendamento date,
     constraint fk_agendamento_paciente foreign key(paciente_id) references paciente(id)
 );
 
 create table if not exists atendimento(
-    id int(7) unsigned not null primary key,
-    agendamento_id  int(7) unsigned not null,
+    id int(7) unsigned not null auto_increment primary key,
+    agendamento_id int(7) unsigned not null,
     atendente_id int(7) unsigned not null,
-    data_confirmacao  date,
-    constraint fk_atendimento_agendamento foreign key(agendamento_id) references agendamento(id),
-    constraint fk_atendimento_atendente foreign key(atendente_id) references atendente(id)
+    data_confirmacao date,
+    constraint fk_atendimento_atendente foreign key(atendente_id) references atendente(id),
+    constraint fk_atendimento_agend foreign key(agendamento_id) references agendamento(id)
 );
-
