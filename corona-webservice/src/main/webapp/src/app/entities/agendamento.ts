b0b3a27b44443eaecd22b3525d5bd7d51d9663ca
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2020-04-21 23:16:06 -0300

    filipe_merge Adicionado entidades para o front end

--- corona-webservice/src/main/webapp/src/app/entities/agendamento.ts
+++ corona-webservice/src/main/webapp/src/app/entities/agendamento.ts
@@ -1,2 +1,6 @@
+import {Paciente} from "./paciente";
+
 export class Agendamento {
+  paciente: Paciente;
+  dataAgendamento?: Date;
 }
--- corona-webservice/src/main/webapp/src/app/entities/endereco.ts
+++ corona-webservice/src/main/webapp/src/app/entities/endereco.ts
@@ -1,2 +1,9 @@
 export class Endereco {
+  rua: string;
+  numero: string;
+  completo?: string;
+  cep?: string;
+  bairro?: string;
+  cidade?: string;
+  estado?: string;
 }
